6d9c7aa8bb7f172ff9fc985d9a93b92db4fc39ae
Jonathan Vilaça <54476678+jonathan-vilaca@users.noreply.github.com>
2020-03-19 11:33:46 -0300

    Update Calculadora.py

--- Calculadora.py
+++ Calculadora.py
@@ -4,11 +4,14 @@ sinal = input("Insira o sinal da operacao: ")
 numero_b = int(input("Insira o segundo numero: "))
 if sinal == "+":
     print("Resultado é:", numero_a + numero_b)    
-if sinal == "-":
-    print("Resultado é:", numero_a - numero_b)
-if sinal == "*":
-    print("Resultado é:", numero_a * numero_b)
-if sinal == "/":
-    print("Resultado é:", numero_a / numero_b)
 else:
+    if sinal == "-":
+        print("Resultado é:", numero_a - numero_b)
+    else:
+        if sinal == "*":
+            print("Resultado é:", numero_a * numero_b)
+        else:
+            if sinal == "/":
+                print("Resultado é:", numero_a / numero_b)
+if (sinal != "+") and (sinal != "-") and (sinal = "*") and (sinal != "/"):
     print("Insira apenas sinais de operação básica")
