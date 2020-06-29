b3d1433a6c7668df6b39cab195c929c7923cec4b
Jonathan Vilaça <54476678+jonathan-vilaca@users.noreply.github.com>
2020-06-29 15:44:37 -0300

    Add files via upload

---
+++ biblioteca.py
@@ -0,0 +1,103 @@
+livros_disponíveis = []
+livros_emprestados = []
+usuarios = []
+bibliotecarios = []
+
+print(30 * '-')
+print("BIBLIOTECA EPSA")
+print(30 * '-')
+print("")
+
+
+def opcoes():
+    print("")
+    print("O que faremos agora?")
+    print("")
+    print("Cadastro de usuário? Digite 1")
+    print("Cadastro de livros novos? Digite 2")
+    print("Cadastro de novos bibliotecários? Digite 3")
+    print("Empréstimos de livros? Digite 4")
+    print("Devolução de livros? Digite 5")
+    print("Relatório de livros emprestasdos e disponíveis? Digite 6")
+    print("")
+
+opcoes()
+
+
+def reinicio():
+    resposta = input("Deseja mais alguma coisa? (S) ou (N) ")
+    if resposta.upper() == 'S':
+        opcoes()
+        servico()
+    elif resposta.upper() == 'N':
+        print("Ok, até a próxima! 👋")
+    else:
+        print("Favor inserir uma entrada válida! (S) ou (N)")
+        print("")
+        reinicio()
+
+
+def usuario():
+    novo_usuario = input("Insira o nome do novo usuário: ")
+    usuarios.append(novo_usuario)
+    print("Usuário cadastrado com sucesso!")
+    reinicio()
+
+
+def livros():
+    novo_livro = input("Insira o nome do novo livro: ")
+    livros_disponíveis.append(novo_livro)
+    print("Livro cadastrado com sucesso!")
+    reinicio()
+
+
+def novos_bibliotecarios():
+    novo_biblio = input("Insira o nome do novo bibliotecário: ")
+    bibliotecarios.append(novo_biblio)
+    print("Bibliotecário cadastrado com sucesso!")
+    reinicio()
+
+
+def emprestimo_livro():
+    emprestimo = input("Qual livro será emprestado? ")
+    livros_disponíveis.remove(emprestimo)
+    livros_emprestados.append(emprestimo)
+    reinicio()
+
+def devolucao_de_livros():
+    devolucao = input("Qual livro será devolvido? ")
+    if devolucao in livros_emprestados:
+        livros_emprestados.remove(devolucao)
+        livros_disponíveis.append(devolucao)
+    else:
+        print("Este livro não pertence a esta biblioteca!")
+        reinicio()
+
+
+def relatorio_completo():
+    print(f'Os livros de nosso acervo são:')
+    print("")
+    print(f"{livros_disponíveis} estão disponíveis para empréstimo")
+    print("")
+    print(f"E estes: {livros_emprestados}, não estão disponíveis para empréstimos pois já estão emprestados.")
+
+
+def servico():
+    tipo_de_servico = input("O que vc deseja? ")
+    if tipo_de_servico == '1':
+        usuario()
+    elif tipo_de_servico == '2':
+        livros()
+    elif tipo_de_servico == '3':
+        novos_bibliotecarios()
+    elif tipo_de_servico == '4':
+        emprestimo_livro()
+    elif tipo_de_servico == '5':
+        devolucao_de_livros()
+    elif tipo_de_servico == '6':
+        relatorio_completo()
+    else:
+        print("Favor inserir uma entrada válida! De 1 a 6")
+        servico()
+
+servico()
