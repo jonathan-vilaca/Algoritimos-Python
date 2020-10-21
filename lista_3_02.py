ae83b47114d672e745baa0ca1b5015f2b7858d98
Jonathan Vilaça <54476678+jonathan-vilaca@users.noreply.github.com>
2020-10-21 11:20:49 -0300

    Delete lista_3_02.py

--- lista_3_02.py
+++
@@ -1,8 +0,0 @@
-#2) Faça um algoritmo para ler dois números inteiros e informar se estes números são iguais ou diferentes.
-print("IGUAIS OU NÃO")
-numero1 = int(input("Insira o primeiro número:"))
-numero2 = int(input("Insira o segundo número:"))
-if numero1 == numero2:
-    print("OS NÚMEROS SÃO IGUAIS!")
-else:
-    print("OS NÚMEROS SÃO DIFERENTES!")
