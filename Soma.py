f2233bf174ecd521f8a39e85055fb88981e69330
Jonathan Vilaça <54476678+jonathan-vilaca@users.noreply.github.com>
2020-10-21 11:38:23 -0300

    Delete Soma.py

--- Soma.py
+++
@@ -1,7 +0,0 @@
-print("Some:")
-retangulo_a = input("Informe o primeiro número: ")
-retangulo_b = input("Informe o segundo número: ")
-retangulo_a = int(retangulo_a)
-retangulo_b = int(retangulo_b)
-retangulo_c = retangulo_a + retangulo_b
-print("A soma é", retangulo_c)
