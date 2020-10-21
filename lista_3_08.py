8d528fd6a77d1d4904d5c0b1f0699c6de8d15c1a
Jonathan Vilaça <54476678+jonathan-vilaca@users.noreply.github.com>
2020-10-21 11:27:57 -0300

    Delete lista_3_08.py

--- lista_3_08.py
+++
@@ -1,6 +0,0 @@
-#8) Elabore um algoritmo para testar se uma senha digita é igual a “batatafrita”. Se a senha estiver correta escreva “Acesso permitido”, do contrário emita a mensagem “Você não tem acesso ao sistema”.
-senha = input("Insira sua senha:")
-if senha == "batatafrita":
-    print("Acesso permitido!")
-else:
-    print("Você não tem acesso ao sistema!")
