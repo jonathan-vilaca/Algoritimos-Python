0ef425d2a19ea27aae39d4cd4aa4dbba497983e7
Jonathan Vilaça <54476678+jonathan-vilaca@users.noreply.github.com>
2020-10-21 11:37:17 -0300

    Delete maior_10.py

--- maior_10.py
+++
@@ -1,9 +0,0 @@
-print("Veja se é maior que 10")
-entrada = int(input("Digite o número: "))
-if entrada > 10:
-    print("Maior que 10!")
-else:
-    if entrada == 10:
-        print("Igual a 10!")
-    else:
-        print("Menor que 10!")
