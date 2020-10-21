9b2648f59dbddb87f3a57305de1ef8f8331d0a35
Jonathan Vilaça <54476678+jonathan-vilaca@users.noreply.github.com>
2020-10-21 11:47:16 -0300

    Create soma_todos_1_a_100.py

---
+++ soma_todos_1_a_100.py
@@ -0,0 +1,7 @@
+contador = 1
+soma = 0
+while contador <= 100:
+    print(f"{contador}")#lista os números de 1 a 100
+    soma = soma + contador
+    contador = contador + 1
+print(f"A soma total dos núemros são:{soma}")#imprime o a soma dos números de 1 a 100
